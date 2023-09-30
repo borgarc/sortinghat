96c3274e8c42b6e193435518cb80315783e7ce7e
borgarc <borjagarciavillanueva@gmail.com>
2023-09-30 20:44:50 +0200

    created view for the selection chat

---
+++ sorting-hat/src/views/SelectionChat.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <div v-for="question in questions" :key="question.title">
+      <span>{{ question.title }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import seeds from '../assets/sorting_hat.json'
+
+export default {
+  name: 'SelectionChat',
+  data () {
+    return {
+      questions: seeds
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
